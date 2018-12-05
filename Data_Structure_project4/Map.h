2b923765b001df1c87dee6d21a8275e049dc5dd7
jijorbq <86359005@qq.com>
2018-12-05 19:35:24 +0800

    Finish Map.cpp 1.0

--- Data_Structure_project4/Map.h
+++ Data_Structure_project4/Map.h
@@ -9,11 +9,15 @@
 #include <utility>
 #include <string>
 #include <tuple>
+//#define che
+#define debug(...) fprintf(stderr,__VA_ARGS__)
+
+using std::vector;
 
 enum Type_node { Place, Point, Restaurant, WC, Parking_lot, Smarket };
 
 typedef std::pair<int, int> Coordinate;
-
+const int N= 100;const double INF=1e9, RADIUS= 10;
 struct Node
 {
 	Coordinate coordinate;
@@ -32,10 +36,12 @@ public:
 	Map();
 	~Map() = default;
 
+	std::vector<Coordinate> shortest_particular(Coordinate from, Coordinate to, int op) ;
+
 	//return the shortest path by driving
-	std::vector<Coordinate> shortest_Navigation_Car(Coordinate from, Coordinate to) const;
+//	std::vector<Coordinate> shortest_Navigation_Car(Coordinate from, Coordinate to) ;
 	//return the shortest path by walking
-	std::vector<Coordinate> shortest_Navigation_Walk(Coordinate from, Coordinate to) const;
+//	std::vector<Coordinate> shortest_Navigation_Walk(Coordinate from, Coordinate to) ;
 
 	std::vector<Node> getPlace() const;
 	std::vector<Node> getWC() const;
@@ -43,10 +49,22 @@ public:
 	std::vector<Node> getSmarket() const;
 	std::vector<Node> getRestaurant() const;
 
-	std::string Info(Coordinate) const;
+	std::string Info(Coordinate) ;
 
-private:
+	Coordinate MatchSite(Coordinate) ;
+    std::vector<Coordinate> shortest_Navigation_Car(
+        const std::vector<Coordinate> &KeySite);
+    std::vector<Coordinate> shortest_Navigation_Walk(
+        	const std::vector<Coordinate> &KeySite);
+// private:
 	std::map<Coordinate, Node> adj_Matrix;
+    void BuildGraph(int n,int op);
+    int Label_Common();
+    int Label_Key(const std::vector<Coordinate> &KeySite);
+    std::vector<Coordinate> shortest_particular(const vector<Coordinate> &KeySite ,int op);
+
+	void Walk_Along_Shortest(int n,int from, int to, std::vector<int> &);
+	double distance(Coordinate , Coordinate);
 };
 
 #endif // !MAP_H
